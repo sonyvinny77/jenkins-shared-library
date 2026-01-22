bd7a6a9ca1072a7e033a2eafb277c97f2e6ce2d5
sonygatlaa <134613431+sonyvinny77@users.noreply.github.com>
2026-01-22 10:46:30 +0530

    Update deployToTomcat.groovy

--- vars/deployToTomcat.groovy
+++ vars/deployToTomcat.groovy
@@ -1,31 +1,31 @@
-def call(Map config) {
-
-    pipeline {
+def call(Map config) 
+{
+    pipeline 
+    {
         agent any
-
-        stages {
-
-            stage("Checkout - ${config.appName}") {
-                steps {
+        stages 
+        {
+            stage("Checkout - ${config.appName}") 
+            {
+                steps 
+                {
                     git branch: 'main',
                         url: config.gitRepo
                 }
             }
-
-            stage("Build WAR - ${config.appName}") {
-                steps {
+            stage("Build WAR - ${config.appName}") 
+            {
+                steps 
+                {
                     sh 'mvn clean package'
                 }
             }
-
-            stage("Deploy to Tomcat - ${config.appName}") {
-                steps {
+            stage("Deploy to Tomcat - ${config.appName}") 
+            {
+                steps 
+                {
                     sh """
-                    sudo systemctl stop tomcat
-
-                    rm -rf /opt/tomcat/webapps/${config.appName}*
                     cp target/*.war /opt/tomcat/webapps/${config.appName}.war
-
                     sudo systemctl start tomcat
                     """
                 }
@@ -33,4 +33,3 @@ def call(Map config) {
         }
     }
 }
-
